0bc4e1fca4c08cff6cfc1cb31a360ca71f3fe84f
gg134679852 <metal852794613@gmail.com>
2021-08-03 13:44:04 +0800

    first-commit

---
+++ src/MovieCard.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import './MovieCard.scss'
+const MovieCard = () => {
+  return (
+    <section className="movie-card-area">
+  <div className="container">
+    <div className="movie-card-wrapper">
+      <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+       <div className="movie-card">
+        <img src="https://movie-list.alphacamp.io/posters/rv1AWImgx386ULjcf62VYaW8zSt.jpg" className="movie-card__img" />
+        <div className="movie-card__info">
+          <h1>sadsdafasdf</h1>
+          <p>sadfasdf</p>
+          <a href="asdasd">read more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default MovieCard
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React,{useState,Fragment} from 'react'
+import './NavBar.scss'
+const NavBar =()=>{
+  return (
+    <header className="header">
+  <div className="web-title">
+    <img className="web-title__img" src="https://www.iamherelearning.com/wp-content/uploads/2020/02/Movie-Icon-1-460x406.png" alt="" />
+    <h1>MOVIE-LIST</h1>
+  </div>
+  <div className="user-page-link">
+    <a href="#"><i className="fas fa-user-circle"> </i>
+    </a>
+  </div>
+ </header>
+  )
+}
+
+export default NavBar
+
+
+
