3dc9921a040bde09a5d878c1abc161f71e19d648
gg134679852 <metal852794613@gmail.com>
2021-08-16 10:18:11 +0800

    update-search-bar

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,22 +2,37 @@ import React,{useState,Fragment} from 'react'
 import MovieCard from './MovieCard'
 import Modal from './Modal'
 import Slider from './Slider'
+import SearchBar from './SearchBar'
 import './Main.scss'
 const Main = ({movieDatas}) => {
   const [showModal, setShowModal] = useState(false)
   const [modalDataId, setModalDataId] = useState('')
-
+  const [searchData, setSearchData] = useState('')
   const openModal = (id) => {
     setModalDataId(prev => prev = Number(id)-1)
     setShowModal(prev => !prev)
   }
-
+  const startSearch = (value)=>{
+    const data= [...movieDatas]
+    const wordFliter = /\w+/g
+    if(wordFliter.test(value)){
+      setSearchData(data.filter(e=>e.subMovieTitle.toLowerCase().match(value.toLowerCase())))
+    }else{
+      setSearchData(data.filter(e=>e.name.indexOf(value,0) !== -1))
+    }
+  }
   return (
     <Fragment>
       <Slider movieDatas={[...movieDatas.slice(0,6)]} openModal={openModal}/>
+      <SearchBar startSearch={startSearch} />
     <section className="movie-card-area">
     <div className="movie-card-wrapper">
-      {movieDatas.map(data=>
+      { 
+        searchData ? 
+        searchData.map(data=>
+       <MovieCard propDatas={data} openModal={openModal} key={data.id}/>
+      )
+      :movieDatas.map(data=>
        <MovieCard propDatas={data} openModal={openModal} key={data.id}/>
       )}
    </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './SearchBar.scss'
-const SearchBar = ()=>{
+const SearchBar = ({startSearch})=>{
   return (
-    <div>
-     
+    <div className="search-input">
+     <input type="text" onChange={e=>startSearch(e.target.value)} />
     </div>
   )
 }
