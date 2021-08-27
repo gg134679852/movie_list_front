a8da80dc85d4973ee3f3664272d28e56c7baf901
gg134679852 <metal852794613@gmail.com>
2021-08-27 14:18:12 +0800

    update-user-page

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,9 +1,15 @@
 import React from "react"
-
-const UserPage =()=>{
+import './UserPage.scss'
+const UserPage =({userData})=>{
   return (
     <div className="userPage__container">
-     
+     <div className="userPage__user-area">
+       <i className="fas fa-user-circle"> </i>
+       <div className="userPage__user-area__info">
+       <h1>{userData.name}</h1>
+       <h4>{userData.email}</h4>
+       </div>
+     </div>
     </div>
   )
 }
