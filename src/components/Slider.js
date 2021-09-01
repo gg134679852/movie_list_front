8899c65d463ff95213edbb66a471fd10ad4fbc16
gg134679852 <metal852794613@gmail.com>
2021-09-01 16:01:03 +0800

    change-slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{Fragment} from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
 import SwiperCore, {
   Navigation
@@ -7,24 +7,19 @@ import 'swiper/swiper.scss'
 import 'swiper/components/navigation/navigation.scss'
 import './Slider.scss'
 SwiperCore.use([Navigation])
-const Slider = ({movieDatas}) => { 
-  
+const Slider = ({movieDatas}) => {
   return (
   <section className="movie-card-slider-area">
+  <h1>近期上映</h1>
   <Swiper navigation={true} className="swiper">
     {movieDatas.map((data,index)=>(<SwiperSlide key={index} className="swiper-item">
       <div className="swiper-card">
         <img className="swiper-card__img"  src={data.backdrop} alt="" />
         <h3 className="swiper-card__title">{data.name}</h3>
-        <button className="swiper-card__button" id={data.id}>
-          <h4 className="swiper-card__button__text" id={data.id}>觀看預告片
-          </h4>
-        </button>
       </div>
     </SwiperSlide>))}
   </Swiper>
     </section>
   )
 }
-
 export default Slider
